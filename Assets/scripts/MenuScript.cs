12ef5b33d26bef079835daa69e7574af984776c9
Mathieu Mattei (Klash2b) <mattei.mathieu2b1@gmail.com>
2020-04-27 21:27:47 +0200

    v0.1
    - Création & configuration scènes Menu & Level1

---
+++ Assets/scripts/MenuScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// Title screen script
+/// </summary>
+public class MenuScript : MonoBehaviour
+{
+    public void StartGame()
+    {
+        // "Stage1" is the name of the first scene we created.
+        //Application.LoadLevel("Stage1");
+        SceneManager.LoadScene("Level1");
+    }
+}
