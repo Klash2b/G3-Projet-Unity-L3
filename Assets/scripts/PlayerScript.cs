ec4c7b7214e909565b634d5bbb1350455e5c98ff
cecceccu <58193062+cecceccu@users.noreply.github.com>
2020-05-01 19:40:20 +0200

    v0.5
    Scrolling caméra, ramassage pièces, UI nombre de pièces ramassées, suppression des collisions avec le haut des pièces, ajout de variables publiques modifiables dans PlayerScript

--- Assets/scripts/PlayerScript.cs
+++ Assets/scripts/PlayerScript.cs
@@ -10,19 +10,6 @@ public class PlayerScript : MonoBehaviour
   /// <summary>
   /// 1 - La vitesse de déplacement
   /// </summary>
-<<<<<<< Updated upstream
-  public Vector2 speed = new Vector2(60, 60);
-  public float maxSpeed = 20;
-  
-
-  // 2 - Stockage du mouvement
-  private Vector2 movement;
-  private bool estAuSol;
-  private bool saute;
-  private bool toucheSautEnfoncee;
-  private float forceSaut = CalculSaut(9.81f, 60.0f);
-
-=======
   public Vector2 vitesse = new Vector2(80, 80);
   public float vitesseMax = 25;
 
@@ -37,18 +24,13 @@ public class PlayerScript : MonoBehaviour
   private bool saute;
   private bool toucheSautEnfoncee;
 
->>>>>>> Stashed changes
   private float distanceSol;
 
  void Start()
  {
    distanceSol = GetComponent<BoxCollider2D>().bounds.extents.y;
-<<<<<<< Updated upstream
-   estAuSol = false;;
-=======
    estAuSol = false;
    gravite = Physics2D.gravity.magnitude;
->>>>>>> Stashed changes
  }
 
   void Update()
@@ -60,11 +42,7 @@ public class PlayerScript : MonoBehaviour
     
 
     // 4 - Calcul du mouvement et de la direction du sprite
-<<<<<<< Updated upstream
-    movement = new Vector2(speed.x * inputX, speed.y * inputY);
-=======
     movement = new Vector2(vitesse.x * inputX, vitesse.y * inputY);
->>>>>>> Stashed changes
 
     float scaleX = transform.localScale.x;
     float scaleY = transform.localScale.y;
@@ -92,11 +70,7 @@ public class PlayerScript : MonoBehaviour
       if(estAuSol)
       {
         saute = true;
-<<<<<<< Updated upstream
-        GetComponent<Rigidbody2D>().AddForce(Vector2.up * 1f * forceSaut * GetComponent<Rigidbody2D>().mass,
-=======
         GetComponent<Rigidbody2D>().AddForce(Vector2.up * 1f * CalculSaut(gravite, hauteurSaut) * GetComponent<Rigidbody2D>().mass,
->>>>>>> Stashed changes
          ForceMode2D.Impulse);
       }
     }
@@ -113,11 +87,7 @@ public class PlayerScript : MonoBehaviour
     
     GetComponent<Rigidbody2D>().AddForce(movement);
     
-<<<<<<< Updated upstream
-    if (Mathf.Abs(GetComponent<Rigidbody2D>().velocity.x) > maxSpeed
-=======
     if (Mathf.Abs(GetComponent<Rigidbody2D>().velocity.x) > vitesseMax
->>>>>>> Stashed changes
     && Input.GetAxis("Horizontal")*GetComponent<Rigidbody2D>().velocity.x>0)
     /*La 2ème condition permet de vérifier que la direction de l'input est identique
     à la direction actuelle de déplacement. Cela évite une perte instantanée de la vélocité
@@ -126,25 +96,6 @@ public class PlayerScript : MonoBehaviour
     {
 
       Vector2 vect = GetComponent<Rigidbody2D>().velocity;
-<<<<<<< Updated upstream
-      vect.x = Input.GetAxis("Horizontal")*maxSpeed;
-      GetComponent<Rigidbody2D>().velocity = vect;
-    }
-  }
-
-  void LateUpdate()
-  {
-    if(saute)
-    {
-      if(!toucheSautEnfoncee && GetComponent<Rigidbody2D>().velocity.y>0)
-      {
-          GetComponent<Rigidbody2D>().AddForce(-1.5f * Vector2.up * forceSaut * GetComponent<Rigidbody2D>().mass);
-      }
-    }  
-
-    if (estAuSol && GetComponent<Rigidbody2D>().velocity.y<0)
-    {
-=======
       vect.x = Input.GetAxis("Horizontal")*vitesseMax;
       GetComponent<Rigidbody2D>().velocity = vect;
     }
@@ -162,18 +113,13 @@ public class PlayerScript : MonoBehaviour
 
     if (estAuSol && GetComponent<Rigidbody2D>().velocity.y<0)
     {
->>>>>>> Stashed changes
       Vector2 v = GetComponent<Rigidbody2D>().velocity;
       v.y = 0;
       GetComponent<Rigidbody2D>().velocity = v;
     }
   }
 
-<<<<<<< Updated upstream
-  public static float CalculSaut(float gravite, float hauteur)
-=======
   public float CalculSaut(float gravite, float hauteur)
->>>>>>> Stashed changes
   {
     return Mathf.Sqrt(2 * gravite * hauteur);
   }
@@ -184,9 +130,4 @@ public class PlayerScript : MonoBehaviour
   }
 
   
-<<<<<<< Updated upstream
 }
-
-=======
-}
->>>>>>> Stashed changes
