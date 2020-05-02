6f5ce807b773b04a64df988f5c90d2d477ddbf32
cecceccu <58193062+cecceccu@users.noreply.github.com>
2020-05-02 20:56:26 +0200

    v0.6
    Ajout d'ennemis type "goomba", avec déplacements linéaires sur une même plateforme, et changeant de direction quand la fin de la plateforme est atteinte

--- Assets/scripts/MoveScript.cs
+++ Assets/scripts/MoveScript.cs
@@ -22,19 +22,57 @@ public class MoveScript : MonoBehaviour
     private Vector2 movement;
     private Rigidbody2D rigidbodyComponent;
 
+    private float distanceSol;
+
     void Update()
     {
         // 2 - Movement
         movement = new Vector2(
           speed.x * direction.x,
           speed.y * direction.y);
+        distanceSol = GetComponent<BoxCollider2D>().bounds.extents.y;
     }
 
     void FixedUpdate()
     {
+
+        float scaleX = transform.localScale.x;
+        float scaleY = transform.localScale.y;
+        float scaleZ = transform.localScale.z;
+
+
         if (rigidbodyComponent == null) rigidbodyComponent = GetComponent<Rigidbody2D>();
 
         // Apply movement to the rigidbody
-        rigidbodyComponent.velocity = movement;
+        if (finPlateforme())
+        {
+            direction.x = -1f*direction.x;
+            rigidbodyComponent.velocity = new Vector2(-movement.x, rigidbodyComponent.velocity.y);
+        }
+        else
+        {
+            rigidbodyComponent.velocity = new Vector2(movement.x, rigidbodyComponent.velocity.y);
+        }
+
+        if (direction.x > 0)
+        {   
+            transform.localScale = new Vector3(Mathf.Abs(scaleX),
+             scaleY, scaleZ);
+        }
+    
+        else if (direction.x < 0)
+        {
+            transform.localScale = new Vector3(-1f*Mathf.Abs(scaleX),
+             scaleY, scaleZ);
+        }
+        
+        
+    }
+
+    public bool finPlateforme()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position+new Vector3(Mathf.Sign(direction.x)*0.05f,0,0),
+         -1f*Vector2.up, distanceSol+0.1f);
+        return !(hit && hit.transform.tag != "Player");
     }
 }
