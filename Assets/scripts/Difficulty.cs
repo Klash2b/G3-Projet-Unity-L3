54a1bf4ca6f0bdc06fc12833b2caddc0613fe227
cecceccu <58193062+cecceccu@users.noreply.github.com>
2020-05-20 23:46:56 +0200

    Niveaux de difficulté
    Ajout de 3 boutons permettant de choisir une difficulté easy, normal ou hard (choix de la diffculté avec une sorte de "toggle", puis validation via le bouton "start game").
    
    Selon la difficulté choisie, certains checkpoints n'apparaissent pas dans les niveaux.
    
    Ajout du code permettant la désactivation de ces checkpoints par l'appui sur les boutons.

---
+++ Assets/scripts/Difficulty.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Difficulty
+{
+    private static string difficulty = "normal";
+
+    public static void setDifficulty(string s)
+    {
+        difficulty = s;
+    }
+
+    public static string getDifficulty()
+    {
+        return difficulty;
+    }
+}
--- Assets/scripts/MenuScript.cs
+++ Assets/scripts/MenuScript.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 /// <summary>
@@ -8,10 +9,43 @@ using UnityEngine.SceneManagement;
 /// </summary>
 public class MenuScript : MonoBehaviour
 {
+    private static string difficulty = "normal";
     public void StartGame()
     {
         // "Stage1" is the name of the first scene we created.
         //Application.LoadLevel("Stage1");
         SceneManager.LoadScene("Level1");
     }
+
+    public void easy()
+    {
+        difficulty = "easy";
+        GameObject.Find("Button Easy").GetComponent<Button>().interactable = false;
+        GameObject.Find("Button Normal").GetComponent<Button>().interactable = true;
+        GameObject.Find("Button Hard").GetComponent<Button>().interactable = true;
+    }
+
+    public void normal()
+    {
+        difficulty = "normal";
+        GameObject.Find("Button Easy").GetComponent<Button>().interactable = true;
+        GameObject.Find("Button Normal").GetComponent<Button>().interactable = false;
+        GameObject.Find("Button Hard").GetComponent<Button>().interactable = true;
+    }
+
+    public void hard()
+    {
+        difficulty = "hard";
+        GameObject.Find("Button Easy").GetComponent<Button>().interactable = true;
+        GameObject.Find("Button Normal").GetComponent<Button>().interactable = true;
+        GameObject.Find("Button Hard").GetComponent<Button>().interactable = false;
+    }
+
+    public static string getDifficulty()
+    {
+        return difficulty;
+    }
+
+
+
 }
